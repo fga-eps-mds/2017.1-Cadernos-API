07508793297d51ea0c1e743ecf3bc52fce3ab035
waysman <waysman@gmail.com>
2017-04-03 19:06:35 +0000

    adding gem rails-controller-testing and minor validation

--- Gemfile
+++ Gemfile
@@ -39,6 +39,6 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
-
+gem 'rails-controller-testing'
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 #gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- spec/routing/categories_routing_spec.rb
+++ spec/routing/categories_routing_spec.rb
@@ -6,18 +6,18 @@ RSpec.describe CategoriesController, type: :routing do
     it "routes to #index" do
       expect(:get => "/categories").to route_to("categories#index")
     end
-
-    it "routes to #new" do
-      expect(:get => "/categories/new").to route_to("categories#new")
-    end
+  
+  #  it "routes to #new" do
+  #    expect(:get => "/categories/new").to route_to("categories#new")
+  #  end
 
     it "routes to #show" do
       expect(:get => "/categories/1").to route_to("categories#show", :id => "1")
     end
 
-    it "routes to #edit" do
-      expect(:get => "/categories/1/edit").to route_to("categories#edit", :id => "1")
-    end
+    #it "routes to #edit" do
+    #  expect(:get => "/categories/1/edit").to route_to("categories#edit", :id => "1")
+    #end
 
     it "routes to #create" do
       expect(:post => "/categories").to route_to("categories#create")
