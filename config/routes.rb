21cbaff11508b4defc242a5d5f10ed3be34e8faa
waysman <waysman@gmail.com>
2017-04-03 14:15:46 +0000

    Category class created

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'users/:id/books' => 'users#books'
 
   get 'users/:id/books/:title' => 'books#show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :categories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -31,4 +31,11 @@ ActiveRecord::Schema.define(version: 20170403173015) do
     t.datetime "updated_at",         null: false
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
 end
