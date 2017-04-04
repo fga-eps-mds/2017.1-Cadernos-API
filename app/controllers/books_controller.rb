9c1c230c8389248cf9cc7d0af231252b341dd586
lucasssm <lucas50xd@hotmail.com>
2017-04-04 15:07:08 +0000

    fixing ambiguity of book controllers and implementing book show def

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,20 @@
 class BooksController < ApplicationController
-
-  def index
-    @books = Book.all
-
-    render json: @books
-  end
+  skip_before_action :authenticate_request
+  before_action :set_both, only: [:show, :update, :destroy]
+#  def index
+#    @books = Book.all
+#
+#    render json: @books
+#  end
 
   def show
-    render json: @books
+
+  render json: @book
+#    render json: @books
   end
 
+  def set_both
+    @user = User.find(params[:id])
+    @book = @user.books.find_by_title(params[:title])
+  end
 end
