35dc78d0f253c8406c05ed172027ac43496a6f88
saraivinha97 <geovannessaraiva97@gmail.com>
2017-04-06 20:17:07 +0000

    testando index e show

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,29 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
-  describe "GET /books" do
-  
-  
-  # let(:valid_attributes) {
-  #   {:name => "ValidName",
-  #   :email => "validEmail@mail.com",
-  #   :password => "validpassword",
-  #   :password_confirmation => "validpassword",
-  #   :title => "ValidTitle",
-  #   :user_id => 2,}
-  # }
 
-  it 'should render the show template' do
-    book = Book.create
-    get :index
-    expect(assigns(:books)).to eq([book])
+  before(:all) do
+    @user = User.create! :name => "ValidName",
+                         :email => "validEmail@mail.com",
+                         :password => "validpassword",
+                         :password_confirmation => "validpassword"
   end
-  # describe "GET #index" do
-  #   it "assigns all books as @books" do
-  #     user = User.create! valid_attributes
-  #     book = Book.create! valid_attributes
-  #     get :index, params: {}
-  #     expect(assigns(:books)).to eq([book])
-  #   end
+  
+  before(:each) do
+    @book = Book.new :title => "first", :user => @user
+  end
+  
+  describe "GET #index" do
+    it "assigns all books as @books" do
+      expect(@book.save).to be(true)
+      
+      get :index
+      expect(assigns(:books)).to eq([@book])
+    end
+  end
+  
+  describe "GET #show" do
+    it "assings book as @book" do
+      expect(@book.save).to be(true)
+      get :show, :params => {id: @book.id}
+      expect(assigns(:book)).to eq(@book)
+    end
   end
 end
