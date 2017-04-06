5b70e753b0790db3394dbe673b43720e28f02421
lucasmidlhey <lucasmidlhey@hotmail.com>
2017-04-06 17:57:27 +0000

    methods update and destroy from book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,38 @@
 class BooksController < ApplicationController
   skip_before_action :authenticate_request
   before_action :set_both, only: [:show, :update, :destroy]
-#  def index
-#    @books = Book.all
-#
-#    render json: @books
-#  end
+  before_action :set_book, only: [:update, :destroy]
+
+  def index
+    @books = Book.all
+
+    render json: @books
+  end
 
   def show
+    @book = Book.find(params [:title])
 
-  render json: @book
+    render json: @book
   end
 
   def set_both
     @user = User.find(params[:id])
+
     @book = @user.books.find_by_title(params[:title])
   end
+
+  def update
+    @book.update(:id)
+  end
+
+  def destroy
+    @book.destroy
+  end
+
+  private
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   get 'users/:id/books' => 'users#books'
 
   get 'users/:id/books/:title' => 'books#show'
+
+  resources :books
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
