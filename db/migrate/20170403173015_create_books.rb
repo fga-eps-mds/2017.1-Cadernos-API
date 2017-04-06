a2454d9a9126e547ef1fca2f9bd05be76c48e5ea
lucasssm <lucas50xd@hotmail.com>
2017-04-06 13:26:13 +0000

    changing user reference from books and adding book models test

--- db/migrate/20170403173015_create_books.rb
+++ db/migrate/20170403173015_create_books.rb
@@ -2,11 +2,9 @@ class CreateBooks < ActiveRecord::Migration[5.0]
   def change
     create_table :books do |t|
       t.timestamps
-      t.string :idBook
       t.string :title
-      t.string :user_id #allowed users enter here?
+      t.references :user, foreign_key: true
       t.string :task_list
     end
-    add_index :books, [:user_id, :created_at]
   end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
+	before(:each) do
+		@user = User.create :name => "usuario", :password => "123456"
+	end
+
+	describe "user_id validations" do
+		it "shouldnt save if theres no user" do
+			@book = Book.new
+			expect(@book.save).to be(false)
+			expect(@book.errors[:user]).to include("can't be blank")
+			@book.user = @user
+			expect(@book.save).to be(true)
+		end
+	end
 end
