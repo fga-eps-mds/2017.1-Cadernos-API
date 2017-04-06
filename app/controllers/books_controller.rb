0895d8108a3c16e13267ad9fe8bc85f7b8b572cb
saraivinha97 <geovannessaraiva97@gmail.com>
2017-04-06 16:54:20 +0000

    add create book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,11 @@
 class BooksController < ApplicationController
   skip_before_action :authenticate_request
   before_action :set_both, only: [:show, :update, :destroy]
-#  def index
-#    @books = Book.all
-#
-#    render json: @books
-#  end
+  def index
+     @books = Book.all
+  
+     render json: @books
+   end
 
   def show
 
@@ -16,4 +16,19 @@ class BooksController < ApplicationController
     @user = User.find(params[:id])
     @book = @user.books.find_by_title(params[:title])
   end
+  
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      render json: @book, status: :created, location: @book
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :idNumber)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :users
+  resources :books
   get '/users/:id', to: 'users#show'
 
   post 'authenticate', to: 'authentication#authenticate'
 
-  get 'users/:id/books' => 'users#books'
+  get 'users/:id/books' => 'users#books'  
 
   get 'users/:id/books/:title' => 'books#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
