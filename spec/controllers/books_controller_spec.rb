d348df22eb4ad950c96dd436cb0f62a9345284c0
saraivinha97 <geovannessaraiva97@gmail.com>
2017-04-06 19:13:33 +0000

    testing controller

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -3,10 +3,27 @@ require 'rails_helper'
 RSpec.describe BooksController, type: :controller do
   describe "GET /books" do
   
-    it "gives all books" do
-      book = Book.create
-      get :index
-      expect(assigns(:books)).to eq([book])
-    end
+  
+  # let(:valid_attributes) {
+  #   {:name => "ValidName",
+  #   :email => "validEmail@mail.com",
+  #   :password => "validpassword",
+  #   :password_confirmation => "validpassword",
+  #   :title => "ValidTitle",
+  #   :user_id => 2,}
+  # }
+
+  it 'should render the show template' do
+    book = Book.create
+    get :index
+    expect(assigns(:books)).to eq([book])
+  end
+  # describe "GET #index" do
+  #   it "assigns all books as @books" do
+  #     user = User.create! valid_attributes
+  #     book = Book.create! valid_attributes
+  #     get :index, params: {}
+  #     expect(assigns(:books)).to eq([book])
+  #   end
   end
 end
