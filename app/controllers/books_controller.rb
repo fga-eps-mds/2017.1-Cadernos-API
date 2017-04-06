600e0a16242ea446970cda985b04b5d3039fe13c
Geovanne Santos <geovannessaraiva97@gmail.com>
2017-04-06 16:56:33 -0300

    fix routes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,17 @@
 class BooksController < ApplicationController
   skip_before_action :authenticate_request
-  before_action :set_both, only: [:show, :update, :destroy]
-  before_action :set_book, only: [:update, :destroy]
+  before_action :set_book, only: [:show, :update, :destroy]
 
   def index
     @books = Book.all
-
     render json: @books
   end
 
   def show
-    @book = Book.find(params [:title])
-
+    @book = Book.find params[:id]
     render json: @book
   end
+<<<<<<< HEAD
 
   def set_both
     @user = User.find(params[:id])
@@ -21,6 +19,9 @@ class BooksController < ApplicationController
     @book = @user.books.find_by_title(params[:title])
   end
 
+=======
+  
+>>>>>>> 8ae95d7... fix routes
   def create
     @book = Book.new(book_params)
 
@@ -31,25 +32,36 @@ class BooksController < ApplicationController
     end
   end
 
-  def book_params
-    params.require(:book).permit(:title, :idNumber)
-  end
-
   def update
-    @book.update(:id)
+    if @book.update(book_params)
+      render json: @book
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    @book.destroy
+    render json: @book.destroy
   end
 
   private
     def set_book
       @book = Book.find(params[:id])
     end
+<<<<<<< HEAD
 
+=======
+    
+>>>>>>> 8ae95d7... fix routes
     def set_both
       @user = User.find(params[:id])
       @book = @user.books.find_by_title(params[:title])
     end
+<<<<<<< HEAD
+=======
+    
+    def book_params
+      params.require(:book).permit(:title)
+    end
+>>>>>>> 8ae95d7... fix routes
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :users
   resources :books
-  get '/users/:id', to: 'users#show'
-
+  
   post 'authenticate', to: 'authentication#authenticate'
 
+<<<<<<< HEAD
   get 'users/:id/books' => 'users#books'
 
   get 'users/:id/books/:title' => 'books#show'
@@ -13,4 +13,8 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+=======
+  get 'users/:id/books' => 'users#books'  
+  
+>>>>>>> 8ae95d7... fix routes
 end
