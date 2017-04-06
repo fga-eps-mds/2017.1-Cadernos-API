79741112e4fab2b9e3f6609870b2c18a02851c40
saraivinha97 <geovannessaraiva97@gmail.com>
2017-04-06 18:21:36 +0000

    install controller test

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
-
+  describe "GET /books" do
+  
+    it "gives all books" do
+      book = Book.create
+      get :index
+      expect(assigns(:books)).to eq([book])
+    end
+  end
 end
