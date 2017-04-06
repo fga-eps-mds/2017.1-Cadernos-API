42c8dc38141ec614661f434f6f9b7d541efd6318
lucasmidlhey <lucasmidlhey@hotmail.com>
2017-04-06 19:26:59 +0000

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: 'abc', email: 'emailTest', password_digest: '123456', idNumber: '1')
+User.create(name: 'dfg', email: 'emailTest1', password_digest: '1234567', idNumber: '2')
+
+Book.create(title: 'testLivro', user_id: '1')
