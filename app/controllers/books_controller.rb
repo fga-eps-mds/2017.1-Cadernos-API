8f2820a70fe0927d4db97ef7fcd6c9d4e1f43a7b
saraivinha97 <geovannessaraiva97@gmail.com>
2017-04-06 16:54:20 +0000

    add create book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,11 @@
 class BooksController < ApplicationController
   skip_before_action :authenticate_request
   before_action :set_both, only: [:show, :update, :destroy]
-#  def index
-#    @books = Book.all
-#
-#    render json: @books
-#  end
+  def index
+     @books = Book.all
+  
+     render json: @books
+   end
 
   def show
     render json: @books
@@ -15,4 +15,19 @@ class BooksController < ApplicationController
     @user = User.find(params[:id])
     @book = @user.books.find_by_title(params[:title])
   end
+  
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      render json: @book, status: :created, location: @book
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :idNumber)
+  end
+
 end
