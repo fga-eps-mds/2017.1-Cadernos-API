e9e04ef9b2bcfbdb738adc9815bf50439fb38266
victorcmoura <victor_cmoura@hotmail.com>
2017-04-07 00:17:35 +0000

    Authentication using JWT implemented Now it is possible to authenticate users using tokens. Once a json 'POST' containing valid email and password is sent to /authenticate, the server responds with a token valid for a period of time.

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,8 +2,7 @@ class JsonWebToken
   class << self
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, Rails.application.secrets.secret_key_base)[0]
-      HashWithIndifferentAccess.new body
+      JWT.encode(payload, Rails.application.secrets.secret_key_base)
     end
 
     def decode(token)
