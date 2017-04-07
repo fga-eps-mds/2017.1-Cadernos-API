8828f0b5b19a3f7c595bd121effc870854816eeb
waysman <waysman@gmail.com>
2017-04-07 18:07:32 +0000

    add file_list in table tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406202934) do
+ActiveRecord::Schema.define(version: 20170407180520) do
 
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20170406202934) do
     t.integer  "user_id"
     t.index ["created_at"], name: "index_books_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer  "id_category"
+    t.string   "title"
+    t.text     "content"
+    t.integer  "id_user"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "files_list"
   end
 
   create_table "users", force: :cascade do |t|
