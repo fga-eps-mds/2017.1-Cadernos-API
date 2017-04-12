ed32fa329181929c1e2b5b364d78553c5f86e8e4
lucasmidlhey <lucasmidlhey@hotmail.com>
2017-04-12 00:23:24 +0000

    create task_controller spec, fix errors book_controller spec
    Signed-off-by: lucasmidlhey <lucasmidlhey@hotmail.com>

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,7 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy
   validates :user_id, presence: true
+  validates :title,
+            presence: true,
+            length: {in: 5..70}
 end
