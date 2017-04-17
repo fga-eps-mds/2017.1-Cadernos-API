893f5d35545328b3c50e27ba7047eb7ec68db19a
victorcmoura <victor_cmoura@hotmail.com>
2017-04-17 04:22:17 +0000

    [TESTING] Task controller tests

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,32 +1,29 @@
 require 'rails_helper'
 
-
 RSpec.describe TasksController, type: :controller do
+  let(:book){
+    create :book
+  }
 
   let(:task) {
-    create :task
+    create :task, book: book
   }
 
   let(:valid_attributes){
-    {
-    :title => "ValidTitle",
-    :book => book,
-    :user => user
-    }
+    {title: "NewValidTitle", content: "ValidContent", book_id: book.id}
   }
 
-  let(:user){{
-    :email => task.book.user.email,
-    :password => task.book.user.password
-    }}
+  let(:invalid_attributes){
+    {title: "IvT",id_book: book.id, content: ""}
+  }
 
   let(:valid_session){ {} }
 
   describe "GET #index" do
     it "assigns all tasks as @tasks" do
-      @token = AuthenticateUser.call(user[:email], user[:password])
+      expect(task.save).to be(true)
+      @token = AuthenticateUser.call(task.book.user.email, task.book.user.password)
       request.headers["Authorization"] = @token.result
-
       get :index, params: {}, session: valid_session
       expect(assigns(:tasks)).to eq([task])
     end
@@ -34,9 +31,9 @@ RSpec.describe TasksController, type: :controller do
 
   describe "GET #show" do
     it "assigns task as @task" do
-      @token = AuthenticateUser.call(user[:email], user[:password])
+      expect(task.save).to be(true)
+      @token = AuthenticateUser.call(task.book.user.email, task.book.user.password)
       request.headers["Authorization"] = @token.result
-
       get :show, :params => {id: task.id}
       expect(assigns(:task)).to eq(task)
     end
@@ -44,81 +41,66 @@ RSpec.describe TasksController, type: :controller do
 
   describe "POST #create" do
     it "creates a valid task" do
-
-      @token = AuthenticateUser.call(book.user.email, book.user.password)
+      @token = AuthenticateUser.call(task.book.user.email, task.book.user.password)
       request.headers["Authorization"] = @token.result
-
       expect {
-        post :create, params: {task: {title: "NewValidTitle",id_book: book.id, user_id: user.id}}, session: valid_session
+        post :create, params: {task: valid_attributes}, session: valid_session
       }.to change(Task, :count).by(1)
     end
 
     it "won't create a task with invalid attributes" do
-
-      @token = AuthenticateUser.call(book.user.email, book.user.password)
+      @token = AuthenticateUser.call(task.book.user.email, task.book.user.password)
       request.headers["Authorization"] = @token.result
-
       expect {
-        post :create, params: {task: {title: "nope",id_book: book.id, user_id: user.id}}, session: valid_session
+        post :create, params: {task: invalid_attributes}, session: valid_session
       }.to change(Task, :count).by(0)
     end
   end
+
   describe "DELETE #destroy" do
     it "destroys the requested @tasks" do
       expect(task.save).to be(true)
-
-      @token = AuthenticateUser.call(book.user.email, book.user.password)
+      @token = AuthenticateUser.call(task.book.user.email, task.book.user.password)
       request.headers["Authorization"] = @token.result
-
       expect {
         delete :destroy, params: {id: task.id}
       }.to change(Task, :count).by(-1)
       expect(Task.find_by_id(task.id)).to be(nil)
     end
   end
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        {:title => "newValid Title"}
-      }
 
-      let(:invalid_attributes) {
-        {:title => "ivT"}
-      }
+  describe "PUT #update" do
+
+    let(:new_attributes) {
+      {:title => "newValid Title"}
+    }
+
+    context "with valid params" do
 
       it "updates the requested @task" do
-        task = FactoryGirl.create :task
-      @token = AuthenticateUser.call(task.book.user.email, task.book.user.password)
+        expect(task.save).to be(true)
+        @token = AuthenticateUser.call(task.book.user.email, task.book.user.password)
         request.headers["Authorization"] = @token.result
         put :update, params: {id: task.id, task: new_attributes}, session: valid_session
         task.reload
         expect(task.title).to eq(new_attributes[:title])
       end
-
-      context "with invalid params" do
-        it "assigns the book as @book" do
-          task = FactoryGirl.create :task
-          @token = AuthenticateUser.call(task.book.user.email, task.book.user.password)
-          request.headers["Authorization"] = @token.result
-          put :update, params: {id: task.id, task: invalid_attributes}, session: valid_session
-          expect(assigns(:tasks)).to eq(task)
-        end
-      end
-
     end
 
-  end
+    context "with invalid params" do
 
+      let(:invalid_attributes) {
+        {:title => "ivT"}
+      }
 
-  describe "DELETE #destroy" do
-    it "destroys the requested @books" do
-      @token = AuthenticateUser.call(task.book.user.email, task.book.user.password)
-      request.headers["Authorization"] = @token.result
-
-      expect {
-        delete :destroy, params: {id: task.id}
-      }.to change(Task, :count).by(-1)
-      expect(Task.find_by_id(task.id)).to be(nil)
+      it "won't update the task" do
+        expect(task.save).to be(true)
+        @token = AuthenticateUser.call(task.book.user.email, task.book.user.password)
+        request.headers["Authorization"] = @token.result
+        put :update, params: {id: task.id, task: invalid_attributes}, session: valid_session
+        task.reload
+        expect(task.title).not_to eq(invalid_attributes[:title])
+      end
     end
   end
 end
