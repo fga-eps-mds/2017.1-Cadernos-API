e69e0a91f7a4868f76370e356f00be7034aff039
Fábio Teixeira <fabio1079@gmail.com>
2017-05-16 18:52:03 +0000

    Add user to auth verification and make token last 30 days

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,6 +1,6 @@
 class JsonWebToken
   class << self
-    def encode(payload, exp = 24.hours.from_now)
+    def encode(payload, exp = 30.days.from_now)
       payload[:exp] = exp.to_i
       JWT.encode(payload, Rails.application.secrets.secret_key_base)
     end
