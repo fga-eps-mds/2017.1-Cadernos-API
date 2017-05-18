dada593425123808215be8f498a46c88b963e3d4
Fabio Teixeira <fabio1079@gmail.com>
2017-05-17 22:50:41 -0400

    save and display book cover

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   post 'authenticate', to: 'authentication#authenticate'
   get 'users/:id/books' => 'users#books'
 
+  post "/books/:id/cover" => "books#set_cover"
 end
