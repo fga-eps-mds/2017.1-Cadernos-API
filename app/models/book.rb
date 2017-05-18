72c5f31a9992a99941a3047688714d8a0e437008
Fabio Teixeira <fabio1079@gmail.com>
2017-05-18 13:12:13 -0400

    Fix missing cover file location

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,7 +8,8 @@ class Book < ApplicationRecord
   has_attached_file :cover,
                     styles: {medium: "240x120", thumb: "80x40"},
                     content_type: /\Aimage/,
-                    default_url: "/images/:style/missing.jpg"
+                    default_url: "/images/:style/missing.jpg",
+                    default_style: "medium"
 
   validates_attachment :cover
   do_not_validate_attachment_file_type :cover
