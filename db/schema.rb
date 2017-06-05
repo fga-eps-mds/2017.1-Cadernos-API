d5ca3cf6cac04c6a2da1e86153b4d80e4591cc70
pwener <phwener@gmail.com>
2017-06-04 22:41:06 -0300

    Some fixings on seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170602125315) do
-
 
   create_table "books", force: :cascade do |t|
     t.datetime "created_at",         null: false
