e1a88fafcdc1498408c6f146316226dffb7a792d
Gabriel Araújo <gabrieldea.silva@gmail.com>
2017-06-16 13:29:37 -0400

    Upload image on create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,9 +17,7 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
 
     if @task.save
-      @task.save_file(task_params) if params[:task][:document_data]
-      @task.save_image(task_params) if params[:task][:image_data]
-      render json: @task, status: :created, location: @task
+      render :show
     else
       render json: @task.errors, status: :unprocessable_entity
     end
